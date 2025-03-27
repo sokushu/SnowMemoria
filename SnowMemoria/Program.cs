17fa48b224b8b0f83784f392a8b4eb6f6b802723
Sokushu <burpee_soggier0x@icloud.com>
2025-03-27 11:13:56 +0900

    添加 MIT 许可证声明到多个文件

--- SnowMemoria/Program.cs
+++ SnowMemoria/Program.cs
@@ -1,5 +1,28 @@
+﻿// MIT License
+//
+// Copyright(c) 2025 小莕菜
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 using Microsoft.EntityFrameworkCore;
 using SnowMemoria.Database;
+using System.Text;
 
 namespace SnowMemoria
 {
@@ -38,6 +61,22 @@ namespace SnowMemoria
             app.MapRazorPages();
 
             app.Run();
+        }
+    }
+
+    public static class PasswordGenerator
+    {
+        private static readonly char[] _chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+
+        public static string GenerateRandomPassword(int length)
+        {
+            var random = new Random();
+            var password = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                password.Append(_chars[random.Next(_chars.Length)]);
+            }
+            return password.ToString();
         }
     }
 }
